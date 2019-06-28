f2ad56a2338024e3ecfbb2be3cbc7368058d658e
paiva-thiago <paiva.thiago@gmail.com>
2019-06-28 08:42:05 -0300

    npx fix

--- bin/index.js
+++ bin/index.js
@@ -1,30 +1,35 @@
 #!/usr/bin/env node
 
-//To color my card
-const chalk          = require('chalk')
-//To draw the border of my card
-const boxen          = require('boxen')
-//To import the data.json file located in resources folder
-const {readFileSync} = require('fs');
+// To color my card
+const chalk = require('chalk')
+// To draw the border of my card
+const boxen = require('boxen')
 
-//Get The content from a JSON file - You can customize it and create yours here!
-const data = JSON.parse(readFileSync('./resources/data.json'));
-const options = {
-    padding: 2,
-    margin: 1,
-    borderStyle: 'round',
-    dimBorder: true
+// Get The content from JSON  - You can customize it and create yours here!
+const data = {
+  helloWorld: 'HEY YOU!(⌐■_■)',
+  name: 'Thiago Paiva',
+  githubId: 'paiva-thiago',
+  work: 'FullStack Developer',
+  linkedin: 'thiagormp',
+  web: 'http://www.thiagopaiva.com',
+  npx: 'paiva-thiago'
 }
-//Let's color that!
-data.helloWorld = chalk.bgCyan(data.helloWorld);
-data.name       = chalk.bgYellow(data.name);
-data.work       = chalk.gray(data.work);
-data.githubId   = chalk.red('https://github.com/'.concat(data.githubId));
-data.linkedin   = chalk.blue('https://www.linkedin.com/in/'.concat(data.linkedin));
-data.web        = chalk.bgGreen(data.web);
-data.npx        = chalk.bgRed(data.npx);
+const options = {
+  padding: 2,
+  margin: 1,
+  borderStyle: 'round',
+  dimBorder: true
+}
 
-
+// Let's color that!
+data.helloWorld = chalk.bgCyan(data.helloWorld)
+data.name = chalk.bgYellow(data.name)
+data.work = chalk.gray(data.work)
+data.githubId = chalk.red('https://github.com/'.concat(data.githubId))
+data.linkedin = chalk.blue('https://www.linkedin.com/in/'.concat(data.linkedin))
+data.web = chalk.bgGreen(data.web)
+data.npx = chalk.bgRed(data.npx)
 
 const card = `            
                             ${data.helloWorld}                                          
@@ -36,7 +41,6 @@ const card = `
                         Web:  ${data.web}                        
                         
                        Card:  ${data.npx}
-                                                                            `;
+                                                                            `
 
 console.log(chalk.white(boxen(card, options)))
-//console.log(card);
